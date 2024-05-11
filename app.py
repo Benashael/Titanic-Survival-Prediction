a4021bcaea5fca165b47509c0e7fe67aacd4f03d
Ben Ashael <114052702+Benashael@users.noreply.github.com>
2024-05-11 22:10:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Load Streamli page
 st.set_page_config(page_title = "Titanic Survival Prediction", page_icon = ":ship:")
 
 # Load the dataset
